4630e9e127bec64c5af7c4f368399287dc44c3b8
dkim157 <63015027+dkim157@users.noreply.github.com>
2021-03-10 19:57:50 -0800

    basic pandas

--- compare_stock.py
+++ compare_stock.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
 if __name__ == "__main__":
-    data = {'Name': ['Tom', 'Jack', 'Steve', 'Ricky'], 'Age': [28, 34, 29, 42]}
-    df = pd.DataFrame(data)
+    df = pd.read_csv("./T.csv")
     print(df)
