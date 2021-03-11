a7cc69b17b62606b170c5fcf90bebec89f8674ed
dkim157 <63015027+dkim157@users.noreply.github.com>
2021-03-11 13:31:40 -0800

    background table w/o quarterly

--- compare_stock.py
+++ compare_stock.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def format_date(date):
     date = date[5:7] + "/" + date[8:] + "/" + date[2:4]
@@ -21,13 +22,6 @@ def compare_date(date1, date2):
     if day1 == day2: return 0
     else: return day1 - day2
 
-def create_stock_df(csv_file):
-    stock = pd.read_csv(csv_file).drop(['High', 'Low', 'Adj Close', 'Volume'], 1)
-    avg_df = get_avg(stock)
-    stock.insert(3, "Avg", avg_df)
-
-    return stock
-
 def get_avg(stock):
     avg_df = []
 
@@ -36,26 +30,40 @@ def get_avg(stock):
 
     return avg_df
 
-def combine_by_date(stock, div):
+def create_stock_df(csv_file):
+    stock = pd.read_csv(csv_file).drop(['High', 'Low', 'Adj Close', 'Volume'], 1)
+    avg_df = get_avg(stock)
+    stock.insert(3, "Avg", avg_df)
 
-    for index, row in stock.iterrows():
-       print(index, row["Date"])
+    return stock
+
+def combine_by_date(stock, div):
+    # 1. add and sort div dates to stock dates
+    div_dates = div[['Date']].copy()
+    result = stock.append(div_dates).sort_values("Date")
+
+    # 2. add and sort stock dates to div dates
+    stock_dates = stock[['Date']].copy()
+    div = div.append(stock_dates).sort_values("Date")
+    div = div.rename(columns={"Dividends": "Div"}).reset_index(drop=True)
+
+    # 3. merge
+    result = pd.merge(result, div, on="Date", how="outer")
+
+    return result
 
 if __name__ == "__main__":
 
     # create base tables
-    stock = create_stock_df("./att_stock.csv")
-    div = pd.read_csv("./att_div.csv")
+    stock = create_stock_df("./T_stock.csv")
+    div = pd.read_csv("./T_div.csv")
 
-    # sort and reformat dates
+    # sort (and reformat) dates
     stock = stock.sort_values("Date")
     div = div.sort_values("Date")
-    stock["Date"] = stock["Date"].apply(lambda x: format_date(x))
-    div["Date"] = div["Date"].apply(lambda x: format_date(x))
+    #stock["Date"] = stock["Date"].apply(lambda x: format_date(x))
+    #div["Date"] = div["Date"].apply(lambda x: format_date(x))
 
-    # figure out how to actually combine the two
-    frames = [stock, div]
     result = combine_by_date(stock, div)
-    #result = pd.concat(frames)
-
-    #print(result)
+    
+    print(result)
