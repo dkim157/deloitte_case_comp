0ee37ed02b8446d69a7241a64fd9aa20de298595
dkim157 <63015027+dkim157@users.noreply.github.com>
2021-03-11 17:21:23 -0800

    refactored

--- compare_stock.py
+++ compare_stock.py
@@ -1,26 +1,4 @@
 import pandas as pd
-import numpy as np
-
-def format_date(date):
-    date = date[5:7] + "/" + date[8:] + "/" + date[2:4]
-    return date
-
-# -1 if date1 < date2
-# 0 if date1 == date2
-# 1 if date1 > date2
-def compare_date(date1, date2):
-    yr1 = date1[6:]
-    mo1 = date1[:2]
-    day1 = date1[3:5]
-
-    yr2 = date2[6:]
-    mo2 = date1[:2]
-    day2 = date1[3:5]
-
-    if yr1 != yr2: return int(yr1) - int(yr2)
-    if mo1 != mo2: return int(mo1) - int(mo2)
-    if day1 == day2: return 0
-    else: return day1 - day2
 
 def get_avg(stock):
     avg_df = []
@@ -30,7 +8,7 @@ def get_avg(stock):
 
     return avg_df
 
-def create_stock_df(csv_file):
+def open_stock_df(csv_file):
     stock = pd.read_csv(csv_file).drop(['High', 'Low', 'Adj Close', 'Volume'], 1)
     avg_df = get_avg(stock)
     stock.insert(3, "Avg", avg_df)
@@ -52,18 +30,21 @@ def combine_by_date(stock, div):
 
     return result
 
-if __name__ == "__main__":
-
+def create_stock_df(stock_name):
     # create base tables
-    stock = create_stock_df("./T_stock.csv")
-    div = pd.read_csv("./T_div.csv")
+    stock = open_stock_df("./" + stock_name + "_stock.csv")
+    div = pd.read_csv("./" + stock_name + "_div.csv")
 
     # sort (and reformat) dates
     stock = stock.sort_values("Date")
     div = div.sort_values("Date")
-    #stock["Date"] = stock["Date"].apply(lambda x: format_date(x))
-    #div["Date"] = div["Date"].apply(lambda x: format_date(x))
 
     result = combine_by_date(stock, div)
-    
-    print(result)
+    return result
+
+if __name__ == "__main__":
+
+    result = create_stock_df("APPL")
+
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print(result)
