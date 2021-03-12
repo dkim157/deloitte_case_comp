1451e63e7d502db495d01addfa6157cf34803ccc
dkim157 <63015027+dkim157@users.noreply.github.com>
2021-03-12 12:01:08 -0800

    tables done

--- compare_stock.py
+++ compare_stock.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import math
 
 def get_avg(stock):
     avg_df = []
@@ -100,8 +99,9 @@ def add_avg_price(stock, output):
             avg += tmp
             month_ctr += 1
         if (month_ctr == 12) or (i == 0):
-            avg = avg/month_ctr
-            output.loc[index, "Avg Price"] = avg
+            avg = avg/12
+            if(avg != 0):
+                output.loc[index, "Avg Price"] = avg
             index -= 1
             month_ctr = 0
             avg = 0
@@ -116,6 +116,26 @@ def add_change_in_price(output):
         output.loc[i, "Change in Price"] = (new_price-prev_price)/prev_price
         prev_price = new_price
 
+def add_annual_div_yield(output, stock):
+
+    # create list of stock divs
+    div_lst = []
+    for i in range(len(stock)-1, -1, -1):
+        tmp = stock.loc[i, "Div"]
+        if not np.isnan(tmp):
+            div_lst.append(tmp)
+    div_lst = pd.unique(div_lst).tolist()
+
+    for i in range(len(output)-1, 0, -1):
+        output.loc[i, "Div Yield"] = (div_lst.pop(0)*4)/output.loc[i, "Avg Price"]
+
+def add_div_growth_rate(output):
+    prev_price = output.loc[0, "Div Yield"]
+
+    for i in range(1, len(output)):
+        new_price = output.loc[i, "Div Yield"]
+        output.loc[i, "Div Growth Rate"] = (new_price - prev_price) / prev_price
+        prev_price = new_price
 
 def create_out_df(stock, stock_name):
     cols = ['Year', 'Avg Price', 'Change in Price', 'Div Yield', 'Div Growth Rate']
@@ -124,20 +144,30 @@ def create_out_df(stock, stock_name):
     output = add_years(stock, stock_name, output, cols)
     add_avg_price(stock, output)
     add_change_in_price(output)
+    add_annual_div_yield(output, stock)
+    add_div_growth_rate(output)
+
+    output = output.drop(output.index[0]).reset_index(drop=True)
 
     return output
 
+def print_values(name):
+    stock = create_stock_df(name)
+    out = create_out_df(stock, name)
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print(stock)
+        print(out)
+
 if __name__ == "__main__":
 
-    stock_name = "T"
+    stock_names = ["APPL", "IBM", "KO", "NHI", "T"]
 
-    stock = create_stock_df(stock_name)
-    out = create_out_df(stock, stock_name)
+    for name in stock_names:
+        # create dataframes
+        stock = create_stock_df(name)
+        out = create_out_df(stock, name)
+        # create .csv files
+        stock.to_csv(r'./output/' + name + '_full.csv', index=False)
+        out.to_csv(r'./output/' + name + '_summary.csv', index=False)
 
-    stock.to_csv(r'./' + stock_name + '_background.csv', index=False)  # creates a .csv file for background
-    out.to_csv(r'./' + stock_name + '_out.csv', index=False)  # creates the simplified .csv file
-
-    # remove the three hash marks below if you want to print the outputs to terminal
-    #with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-        #print(stock)
-        #print(out)
+    # print_values("APPL")
